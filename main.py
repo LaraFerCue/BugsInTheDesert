73aaa7b6b283b41c12290aa991fd3a8e31fdb597
lara <larafercue@gmail.com>
2018-12-08 14:44:58 +0100

    Setting the title to the window

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pygame
 pygame.init()
 
 display = pygame.display.set_mode()
+pygame.display.set_caption('Bugs on the desert')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
